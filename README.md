e7f516fb48167839354ca288a97e7ba7dd3dd4d1
phlppnhllngr <>
2021-03-19 13:32:48 +0100

    Updated docs, dependencies, no longer rely on winctl to get window handle

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
+# Requirements
+- node.js (v14.16.0) and npm (v6.14.11)
 - Windows 10, MS Word
-- Appium v1.13.0
-- [Windows developer mode](https://docs.microsoft.com/en-us/windows/uwp/get-started/enable-your-device-for-development)
+- [Appium Desktop](https://github.com/appium/appium-desktop/releases) (v1.20.2)
+- enable [Windows developer mode](https://docs.microsoft.com/en-us/windows/uwp/get-started/enable-your-device-for-development) (windows settings &rightarrow; for developers &rightarrow; developer mode)
 - [Windows SDK](https://developer.microsoft.com/en-us/windows/downloads/windows-10-sdk)
 - [Windows Application Driver (WinAppDriver, WAD)](https://github.com/microsoft/WinAppDriver/releases) (v1.1)
 - ```git clone```
-- ```npm install``` (admin)
+- ```npm install```
 - ```npm run servers```
 - ```npm run e2e```
-<br/>
 
+# Errors
+- "Cannot open add-in from localhost" &rightarrow; https://docs.microsoft.com/de-de/office/troubleshoot/office-suite-issues/cannot-open-add-in-from-localhost
+
+# Docs
 - [Word CLI](https://support.office.com/en-us/article/command-line-switches-for-microsoft-office-products-079164cd-4ef5-4178-b235-441737deb3a6)
 - [WinAppDriver API](https://github.com/microsoft/WinAppDriver)
 - [wd API](https://github.com/admc/wd/blob/master/doc/api.md)
+- [Appium driver windows](https://appium.io/docs/en/drivers/windows/)
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const wd = require('wd');
-const winctl = require('winctl');
 const { address, port } = require('./package.json').config;
-const colors = require('colors');
+require('colors');
 const { sleep, assert, toHexString } = require('./helper');
 
 
@@ -19,56 +18,68 @@ const { sleep, assert, toHexString } = require('./helper');
                 : ''
         );
     };
-    ['status', 'command', 'http'].forEach(e => client.on(e, callback)); 
+    ['status', 'command', 'http'].forEach(e => client.on(e, callback));
 
     const desiredCapabilities = {
         platformName: "Windows",
         deviceName: "WindowsPC",
     };
 
-
-
     await sleep(5); // Wait for the window to become available to Appium when Word was launched just before
+
+    // start root session to get Word's window handle
+    desiredCapabilities.app = 'Root';
+    await client.init(desiredCapabilities);
     /**
-     * 
+     * Elements' and windows' names, classes and IDs can be found using inspect.exe from the Windows SDK (https://docs.microsoft.com/en-us/windows/win32/winauto/inspect-objects)
+     * Located under "C:\Program Files (x86)\Windows Kits\10\bin\<version>\x64".
+     * The mapping between attributes as shown by inspect.exe and webdriver locator values can be seen here: https://github.com/microsoft/WinAppDriver
+     */
+    const wordWin = await client.elementByClassName('OpusApp'); // Word's window should be the only one with class "OpusApp"
+    const winHandle = await wordWin.getAttribute("NativeWindowHandle");
+    await client.quit();
+
+    /**
+     * Attach to Word window
      * Assuming "npm run sideload" was run first.
      * Without prior sideloading: Word must be started manually or by replacing appTopLevelWindow with:
      *    desiredCapabilities.app = "WINWORD.EXE",
      *    desiredCapabilities.appArguments = "/q /w"
      * only works if the Word ribbon bar has the launch button for the add-in (sideloading adds this button automatically).
      */
-    const wordwin = await winctl.FindByTitle("Word"); // Assuming there's only 1 window with 'Word' in title.
-    const winHandle = wordwin.getHwnd();
-    desiredCapabilities.appTopLevelWindow = toHexString(winHandle); // Attaching the already opened window to Appium
+    delete desiredCapabilities['app'];
+    desiredCapabilities.appTopLevelWindow = toHexString(Number(winHandle));
     await client.init(desiredCapabilities);
 
-    /**
-     * Elements' names, classes and IDs can be found using inspect.exe from the Windows SDK.
-     * Located under "C:\Program Files (x86)\Windows Kits\10\bin\<version>\x64".
-     * The mapping between attributes as shown by inspect.exe and webdriver locator values can be seen here: https://github.com/microsoft/WinAppDriver
-     */
     const commandsGroup = await client
         .elementByName('MsoDockTop')
         .elementByName('Commands Group'); // value from manifest.xml (CommandsGroup.Label)
-    const addinLauncher = await commandsGroup.elementByName('Show Taskpane'); // value from manifest.xml (TaskpaneButton.Label)
-    await addinLauncher.click();
+    const addInLauncher = await commandsGroup.elementByName('Show Taskpane'); // value from manifest.xml (TaskpaneButton.Label)
+    await addInLauncher.click();
 
     // loading add-in from web server might take a while
     const taskPaneApp = await client.waitForElement(
         'name',
-        'Contoso Task Pane Add-in', // <title> tag value from My Office Add-in/taskpane/taskpane.html
+        'My Office Add-in',
         5000
     );
-    const logo = await taskPaneApp.elementByName('Contoso'); // the <img>'s title attr
-    const isDisplayed = await logo.isDisplayed();
-    assert(isDisplayed, 'The logo should be displayed');
 
-    const runBtn = await taskPaneApp.elementByAccessibilityId('run'); // HTML-id of the RUN-Button
-    await runBtn.click();
+    const logo = await taskPaneApp.elementByName('Contoso'); // the <img>'s title attr
+    const logoIsDisplayed = await logo.isDisplayed();
+    assert(logoIsDisplayed, 'The logo should be displayed');
+
+    const welcome = await taskPaneApp.elementByName('Welcome');
+    const welcomeIsDisplayed = await welcome.isDisplayed();
+    assert(welcomeIsDisplayed, 'The welcome text should be displayed');
+
+    const appBody = await taskPaneApp.elementByAccessibilityId('app-body');
+    const runButton = await appBody.elementByAccessibilityId('run'); // HTML-id of the RUN-Button (inspect.exe: AutomationId)
+    await runButton.click();
+    await sleep(1);
 
     const documentBody = await client.elementByAccessibilityId('Body');
     const text = await documentBody.text();
-    assert(text === 'Hello World\r\r', 'The document\'s text should be equal to "Hello World"');
+    assert(text.includes('Hello World'), `The document's text should contain "Hello World" but was "${text}"`);
 
     console.log('All tests passed'.green);
     await client.quit();
